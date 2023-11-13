3682b4d0720f25db9a38a38699a8c199892c0710
Anthony Bosek <anthony.bosek@gmail.com>
2023-11-13 07:08:01 -0700

    complete task #2

--- server/testing/app_magnitude_test.py
+++ server/testing/app_magnitude_test.py
@@ -6,17 +6,17 @@ from app import app
 
 
 class TestApp:
-    '''Flask application in flask_app.py'''
+    """Flask application in flask_app.py"""
 
     def test_earthquake_magnitude_route(self):
-        '''has a resource available at "/earthquakes/magnitude/<magnitude>".'''
-        response = app.test_client().get('/earthquakes/magnitude/8.0')
+        """has a resource available at "/earthquakes/magnitude/<magnitude>"."""
+        response = app.test_client().get("/earthquakes/magnitude/8.0")
         assert response.status_code == 200
 
     def test_earthquakes_magnitude_match_response(self):
-        '''displays json in earthquake/magnitude route with keys for count, quakes'''
+        """displays json in earthquake/magnitude route with keys for count, quakes"""
 
-        response = app.test_client().get('/earthquakes/magnitude/9.0')
+        response = app.test_client().get("/earthquakes/magnitude/9.0")
         # get the response body
         response_body = response.data.decode()
         # convert to JSON
@@ -40,9 +40,9 @@ class TestApp:
         assert response.status_code == 200
 
     def test_earthquakes_magnitude_no_match_response(self):
-        '''displays json in earthquake/magnitude route with keys for count, quakes'''
+        """displays json in earthquake/magnitude route with keys for count, quakes"""
 
-        response = app.test_client().get('/earthquakes/magnitude/10.0')
+        response = app.test_client().get("/earthquakes/magnitude/10.0")
         # get the response body
         response_body = response.data.decode()
         # convert to JSON
